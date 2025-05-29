5070c1607d900b745d291c50d84f0015b729f186
SohTalotsing-ctrl <sohduclair1@gmail.com>
2025-05-29 17:51:04 +0100

    Last modification of order-service

--- Product-Service/src/main/java/com/example/Product_Service/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Product_Service.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-  public ProductNotFoundException(Integer ProductId) {
-    super("Product not found exception" + ProductId);
-  }
-}
